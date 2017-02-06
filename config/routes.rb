dace0ec2827faa1939ba836c704441bce0130924
Evgeniy28 <azarov91@mail.ru>
2017-02-07 02:21:00 +0300

    user can login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :users
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
+  get    '/signup', to: 'users#new'
+  post   '/signup', to: 'users#create'
+
+  get    '/login',  to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  root 'users#new'
 end
