10ca1768a4f9b36b8384a36f25a810d24eb55ed9
Evgeniy28 <azarov91@mail.ru>
2017-02-15 04:37:19 +0300

    refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: [:destroy]
+  before_action :logged_in_user!, only: [:edit, :update, :destroy]
+  before_action :set_user,        only: [:show, :edit, :update, :destroy]
+  before_action :correct_user,    only: [:edit, :update]
+  before_action :admin_user,      only: [:destroy]
 
   def show
-    @user = User.find(params[:id])
     redirect_to(root_path) and return unless @user.activated?
   end
 
@@ -25,12 +25,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update_attributes(user_params)
       flash[:success] = 'Profile updated!'
       redirect_to @user
@@ -40,8 +37,6 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
-
     @user.destroy
     flash[:success] = 'User deleted!'
     redirect_to root_path
@@ -55,13 +50,9 @@ class UsersController < ApplicationController
 
   # Before filters
 
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless user_logged_in?
-      store_location
-      flash[:danger] = 'Please log in!'
-      redirect_to login_path
-    end
+  # Loads the user
+  def set_user
+    @user = User.find(params[:id])
   end
 
   # Confirms the correct user.
